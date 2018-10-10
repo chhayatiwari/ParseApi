c32d118d55b5762767b290b52116914944c4b027
Chhaya Tiwari <apple@Chhaya-Tiwari.local>
2018-10-10 23:57:16 +0530

    query record from the database

--- ParseApp/ViewController.swift
+++ ParseApp/ViewController.swift
@@ -13,12 +13,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-     let person = PFObject(className: "People")
+    /* let person = PFObject(className: "People")
         person.setValue("Chhaya", forKey: "FirstName")
         person.setValue("Tiwari", forKey: "LastName")
-        person.saveInBackground()
+        person.saveInBackground() */
+      //  saveDataToParse()
+        retreiveDatabase()
     }
 
+    func retreiveDatabase() {
+        let query = PFQuery(className: "People")
+        query.findObjectsInBackground { (objects, error) in
+            if let object = objects {
+                print(object)
+            }
+            else {
+                print(error?.localizedDescription)
+            }
+        }
+    }
+    
+    func saveDataToParse() {
+        let person = PFObject(className: "People")
+        person["FirstName"] = "Sharad"
+        person["LastName"] = "Tiwari"
+        
+        person.saveInBackground()
+    }
 
 }
 
